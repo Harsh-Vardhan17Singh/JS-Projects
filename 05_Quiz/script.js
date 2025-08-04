e9e1c493b59b00209421b08dcbc860aa60d1f492
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2025-08-04 20:03:06 +0530

    Update script.js

--- 05_Quiz/script.js
+++ 05_Quiz/script.js
@@ -51,4 +51,71 @@ document.addEventListener("DOMContentLoaded",()=>{
             Answer:"Udaipur"
         }
     ]
+   let currentQuestionIndex = 0
+    let score = 0
+
+    startBtn.addEventListener('click', startQuiz)
+
+    nextBtn.addEventListener('click', () =>{
+        currentQuestionIndex++
+        if(currentQuestionIndex < questions.length){
+            showQuestion()
+        }else{
+            showResult()
+        }
+
+    })
+
+    restartBtn.addEventListener('click',()=>{
+        currentQuestionIndex  = 0
+        score = 0
+        resultContainer.classList.add('hidden')
+        startQuiz()
+    })
+
+
+    function startQuiz(){
+        startBtn.classList.add('hidden')
+        resultContainer.classList.add('hidden')
+        questionContainer.classList.remove('hidden')
+        showQuestion()
+          
+    }
+
+    function showQuestion(){
+        nextBtn.classList.add('hidden')
+        questionText.textContent = questions[currentQuestionIndex].question
+        choicesList.innerHTML = "" //clear previous choices
+        questions[currentQuestionIndex].choices.forEach(choices =>{
+            const li = document.createElement('li')
+            li.textContent = choices
+            li.addEventListener('click', () => SelectAnswer(choices)) 
+            choicesList.appendChild(li)
+
+
+
+
+
+            //SelectAnswer() would run this function immediately and we don't want it , we want it to execute when the anyone click on it
+
+            //But by using this callback function we are refering to the selectAnswer and also pass parameter to it , it can solve both the instances
+
+        })
+    }
+
+    function SelectAnswer(choice){
+        const correctAnswer = questions[currentQuestionIndex].Answer
+        if(choice == correctAnswer){
+            score++
+        }
+
+        nextBtn.classList.remove('hidden')
+    }
+
+    function showResult(){
+        questionContainer.classList.add('hidden')
+        resultContainer.classList.remove('hidden')
+        scoreDisplay.textContent = `${score} out of ${questions.length}`
+    }
+})
 })
